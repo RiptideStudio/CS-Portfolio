321212277716efe8bebf501173810d33679c469b
RiptideStudio <59582654+RiptideStudio@users.noreply.github.com>
2023-11-14 21:46:58 -0800

    Worked on Navbar and word animation

--- Home/navbar.js
+++ Home/navbar.js
@@ -1,19 +1,18 @@
-var prevScrollpos = window.scrollY || document.documentElement.scrollTop;
+var prevScrollpos = window.pageYOffset || document.documentElement.scrollTop;
 
 window.onscroll = function() {
-  var currentScrollPos = window.scrollY || document.documentElement.scrollTop;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "16px";
+  var currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
+  
+  // If the current scroll position is greater than the previous scroll position, 
+  // the user is scrolling down.
+  if (prevScrollpos < currentScrollPos) {
+    // Slide up and fade out
+    document.getElementById("navbar").style.top = "-50px"; // Adjust if your navbar is taller
+    document.getElementById("navbar").style.opacity = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px"; // Adjust this value to the navbar's height
+    // Slide down and fade in
+    document.getElementById("navbar").style.top = "24px"; // Adjust to the desired position
+    document.getElementById("navbar").style.opacity = "1";
   }
   prevScrollpos = currentScrollPos;
 }
-document.addEventListener('DOMContentLoaded', (event) => {
-  document.querySelectorAll('#navbar a.nav-link').forEach(link => {
-    link.addEventListener('click', (e) => {
-      // Blur the link to remove focus and prevent the :focus effect.
-      setTimeout(() => link.blur(), 0);
-    });
-  });
-});
---
+++ Home/word.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+    const words = document.querySelectorAll('.fade-word');
+    let delay = 250;
+  
+    words.forEach((word, index) => {
+      // Set a timeout for each word, increasing the delay for each subsequent word
+      setTimeout(() => {
+        word.classList.add('show');
+      }, delay);
+  
+      // Increment the delay for the next word
+      delay += 500; // 2000ms = 2 seconds between each word
+    });
+  });
+  
